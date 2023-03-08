77be54e209b2e0c9a69d2732df0c91b76037b7bb
Olena Vdovych <olena.vdovych2@gmail.com>
2023-03-08 14:50:11 +0200

    Changed function findCompartment

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-(function() {
+(function () {
 
     function findCompartment() {
-        const btn = document.getElementById('button');
         const seatNumber = document.getElementById('seatNumber').value;
         const compartmentNumber = Math.ceil(seatNumber / 4);
         const seatType = (compartmentNumber % 2) === 0 ? 'бокове' : 'не бокове';
         const seatPositionCompartment = (compartmentNumber % 4 === 1 || compartmentNumber % 4 === 2) ? 'нижнє' : 'верхнє';
-        // ;
-    
-        window.onload = function() {
-            const result = document.getElementById('resultCompartment');
-            result.innerHTML = 'Дані по вашому квитку: купе № ' + compartmentNumber + ', ' + 'місце ' + seatType + ', ' + 'розташування ' + seatPositionCompartment;
+
+        const result = document.getElementById('resultCompartment');
+        result.innerHTML = 'Дані по вашому квитку: купе № ' + compartmentNumber + ', ' + 'місце ' + seatType + ', ' + 'розташування ' + seatPositionCompartment;
+
+        window.onload = function () {
+            const btn = document.getElementById('button');
+            btn.addEventListener('click', findCompartment);
         }
-    
-    btn.addEventListener('click', findCompartment);   
-    
-}
-findCompartment();
-    
+    }
+    findCompartment();
+
 })();
