12aed0ea365dbf7986d18a1869de41c4d8ff17e7
Olena Vdovych <olena.vdovych2@gmail.com>
2023-03-08 15:07:33 +0200

    Changed findCompartment on click

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 (function () {
 
     function findCompartment() {
-        const seatNumber = document.getElementById('seatNumber').value;
-        const compartmentNumber = Math.ceil(seatNumber / 4);
-        const seatType = (compartmentNumber % 2) === 0 ? 'бокове' : 'не бокове';
-        const seatPositionCompartment = (compartmentNumber % 4 === 1 || compartmentNumber % 4 === 2) ? 'нижнє' : 'верхнє';
+        const btn = document.getElementById('button');
+        btn.addEventListener('click', function () {
+            const seatNumber = document.getElementById('seatNumber').value;
+            const compartmentNumber = Math.ceil(seatNumber / 4);
+            const seatType = (compartmentNumber % 2) === 0 ? 'бокове' : 'не бокове';
+            const seatPositionCompartment = (compartmentNumber % 4 === 1 || compartmentNumber % 4 === 2) ? 'нижнє' : 'верхнє';
 
-        const result = document.getElementById('resultCompartment');
-        result.innerHTML = 'Дані по вашому квитку: купе № ' + compartmentNumber + ', ' + 'місце ' + seatType + ', ' + 'розташування ' + seatPositionCompartment;
-
-        window.onload = function () {
-            const btn = document.getElementById('button');
-            btn.addEventListener('click', findCompartment);
-        }
+            const result = document.getElementById('resultCompartment');
+            result.innerHTML = 'Дані по вашому квитку: купе № ' + compartmentNumber + ', ' + 'місце ' + seatType + ', ' + 'розташування ' + seatPositionCompartment;
+        })
     }
     findCompartment();
 
